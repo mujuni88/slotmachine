d0fdda4fe054c7542c26be6de7507423c3304165
Joe Buza <joebm08@gmail.com>
2016-07-20 21:06:31 -0500

    Cleaned up unused code

--- js/script.js
+++ js/script.js
@@ -7,22 +7,27 @@
         app.onLeverClick();
     });
     
-    function Reel(items, interval, numInterval, selector) {
-        this.items = items;
+    function Reel(interval, numInterval, selector) {
         this.interval = interval || 800;
         this.numInterval = numInterval || 6;
         this.selector = selector;
+        this.intervalId = null;
     }
 
     Reel.prototype.animate = function () {
         var self = this;
-        return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve) {
             var rand = Math.floor(Math.random()*5);
             var numInterval = rand + self.numInterval;
-            var tId = setInterval(function () {
+            
+            if(self.intervalId){
+                clearInterval(self.intervalId);
+            }
+            
+            self.intervalId = setInterval(function () {
                 self.swap();
                 if (--numInterval === 0) {
-                    clearInterval(tId);
+                    clearInterval(self.intervalId);
                     resolve(document.querySelectorAll(self.selector)[1].getAttribute('data'));
                 }
             }, rand + self.interval);
@@ -39,64 +44,9 @@
     };
 
     function SlotMachine() {
-        // reel 1
-        this.coffeeMaker = {
-                name: 'Coffee Maker',
-                type: 'coffee',
-                img: ''
-            };
-            this.teapot = {
-                name: 'Teapot',
-                type: 'tea',
-                img: ''
-            };
-            this.espressoMachine = {
-                name: 'Espresso Machine',
-                type: 'espresso',
-                img: ''
-            };
-        // reel 2 
-        this.coffeeFilter = {
-                name: 'Coffee Filter',
-                type: 'coffee',
-                img: ''
-            };
-            this.teaStrainer = {
-                name: 'Tea Strainer',
-                type: 'tea',
-                img: ''
-            };
-            this.espressoTamper = {
-                name: 'Espresso Tamper',
-                type: 'espresso',
-                img: ''
-            };
-        // reel 3
-        this.coffeeGrounds = {
-                name: 'Coffee Grounds',
-                type: 'coffee',
-                img: ''
-            };
-            this.looseTea = {
-                name: 'Loose Tea',
-                type: 'tea',
-                img: ''
-            };
-            this.espressoBeans = {
-                name: 'Espresso Beans',
-                type: 'espresso',
-                img: ''
-            };
-
-        this.reelOneItems = [this.coffeeMaker, this.teapot, this.espressoMachine];
-        this.reelTwoItems = [this.coffeeFilter, this.teaStrainer, this.espressoTamper];
-        this.reelThreeItems = [this.coffeeGrounds, this.looseTea, this.espressoBeans];
-        
-        var selector = '.reel-item';
-        this.reelOne = new Reel(this.reelOneItems, 200, 11, '#reel1 .slm-reel-item');
-        this.reelTwo = new Reel(this.reelTwoItems,  250, 13, '#reel2 .slm-reel-item');
-        this.reelThree = new Reel(this.reelThreeItems,  300, 12, '#reel3 .slm-reel-item');
-        this.reels = [this.reelOne, this.reelTwo, this.reelThree];
+        this.reelOne = new Reel(200, 11, '#reel1 .slm-reel-item');
+        this.reelTwo = new Reel(250, 13, '#reel2 .slm-reel-item');
+        this.reelThree = new Reel(300, 12, '#reel3 .slm-reel-item');
     }
 
     SlotMachine.prototype.onLeverClick = function () {
@@ -107,12 +57,10 @@
         resLbl.classList.remove('blink');
 
         res.innerHTML = 'Spinning...';
-        console.log('onLeverClick');
         var p1 = self.reelOne.animate(),
             p2 = self.reelTwo.animate(),
             p3 = self.reelThree.animate();
 
-        console.log(p1);
         Promise.all([p1, p2, p3]).then(function(values){
 
             var temp = values[0];
