d44edac8b530c67589d7c46c06869d7eaa17b8fc
Joe Buza <joebm08@gmail.com>
2016-07-20 21:51:17 -0500

    Removed more code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 (function () {
 
-    var app = new SlotMachine(),
-        start = document.getElementById('start');
+    /**************** Application Start ****************/
+    var reelData = [
+        {
+            interval:200,
+            numInterval:11,
+            selector:'#reel1 .slm-reel-item'
+        },
+        {
+            interval:200,
+            numInterval:11,
+            selector:'#reel2 .slm-reel-item'
+        },
+        {
+            interval:200,
+            numInterval:11,
+            selector:'#reel3 .slm-reel-item'
+        }
+    ];
     
-    start.addEventListener('click', function(){
+    var reels = reelData.map(function(data){
+        return new Reel(data.interval, data.numInterval, data.selector);
+    });
+
+    var app = new SlotMachine(reels),
+        start = document.getElementById('start');
+
+    start.addEventListener('click', function () {
         app.onLeverClick();
     });
-    
+
+    /**************** Reel Class ****************/
     function Reel(interval, numInterval, selector) {
         this.interval = interval || 800;
         this.numInterval = numInterval || 6;
@@ -14,66 +38,73 @@
         this.intervalId = null;
     }
 
-    Reel.prototype.animate = function () {
-        var self = this;
-        return new Promise(function (resolve) {
-            var rand = Math.floor(Math.random()*5);
-            var numInterval = rand + self.numInterval;
-            
-            if(self.intervalId){
-                clearInterval(self.intervalId);
-            }
-            
-            self.intervalId = setInterval(function () {
-                self.swap();
-                if (--numInterval === 0) {
-                    clearInterval(self.intervalId);
-                    resolve(document.querySelectorAll(self.selector)[1].getAttribute('data'));
-                }
-            }, rand + self.interval);
-        });
-    };
-    Reel.prototype.swap = function () {
-        var self = this;
+    Reel.prototype.rotateReelItems = function () {
+        var self = this,
+            reelItems = document.querySelectorAll(self.selector),
+            temp = reelItems[0].outerHTML;
 
-        var reelItems = document.querySelectorAll(self.selector);
-        var temp = reelItems[0].outerHTML;
         reelItems[0].outerHTML = reelItems[2].outerHTML;
         reelItems[2].outerHTML = reelItems[1].outerHTML;
         reelItems[1].outerHTML = temp;
     };
 
-    function SlotMachine() {
-        this.reelOne = new Reel(200, 11, '#reel1 .slm-reel-item');
-        this.reelTwo = new Reel(250, 13, '#reel2 .slm-reel-item');
-        this.reelThree = new Reel(300, 12, '#reel3 .slm-reel-item');
+
+    Reel.prototype.animate = function () {
+        var self = this;
+        return new Promise(function (resolve) {
+            var rand = Math.floor(Math.random() * 5),
+                numInterval = rand + self.numInterval,
+                data;
+
+            if (self.intervalId) {
+                clearInterval(self.intervalId);
+            }
+
+            self.intervalId = setInterval(function () {
+                self.rotateReelItems();
+                if (--numInterval === 0) {
+                    clearInterval(self.intervalId);
+                    data = document.querySelectorAll(self.selector)[1].getAttribute('data');
+                    resolve(data);
+                }
+            }, rand + self.interval);
+        });
+    };
+
+    /**************** SlotMachine Class ****************/
+    function SlotMachine(reels) {
+        this.reels = reels;
     }
 
     SlotMachine.prototype.onLeverClick = function () {
         var self = this,
-            res = document.getElementById('result');
-            resLbl = document.querySelector('.slm-line-lbl');
-        
+            res = document.getElementById('result'),
+            resLbl = document.querySelector('.slm-line-lbl'),
+            promises = self.reels.map(function (reel) {
+                return reel.animate();
+            });
+
         resLbl.classList.remove('blink');
-
         res.innerHTML = 'Spinning...';
-        var p1 = self.reelOne.animate(),
-            p2 = self.reelTwo.animate(),
-            p3 = self.reelThree.animate();
 
-        Promise.all([p1, p2, p3]).then(function(values){
-
-            var temp = values[0];
-            for (var i = 1; i < values.length; i++) {
-                if (values[i] !== temp) {
-                    res.innerHTML = '<span class="clr-r">Sorry, spin again</span>';
-                    return;
-                }
-            }
-
-            res.innerHTML = '<span class="clr-g blink">You won free ' + temp + '</span>';
-            resLbl.classList.add('blink');
+        Promise.all(promises).then(function (values) {
+            self.displayResults(values);
         });
     };
+    SlotMachine.prototype.displayResults = function(values){
+        var res = document.getElementById('result'),
+            resLbl = document.querySelector('.slm-line-lbl');
+        
+        var temp = values[0];
+        for (var i = 1; i < values.length; i++) {
+            if (values[i] !== temp) {
+                res.innerHTML = '<span class="clr-r">Sorry, spin again</span>';
+                return;
+            }
+        }
+
+        res.innerHTML = '<span class="clr-g blink">You won free ' + temp + '</span>';
+        resLbl.classList.add('blink');
+    }
 
 })();
